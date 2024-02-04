7f4a6c9bcd8ad331b05c194d6102e298b2e85d49
Prachi Gore <prachigore408@gmail.com>
2024-02-04 22:27:25 +0530

    collison between snake and food added

--- index.js
+++ index.js
@@ -1,71 +1,140 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const gameBoard = document.getElementsByClassName("game-board")[0];
+  const gameBoardSize=600 // gameBoard.offsetWidth // width includes border 
+  console.log('width',gameBoardSize)
+  // const snake=document.getElementsByClassName('snake')[0]
+  // const food=document.getElementsByClassName('food')[0]
+  let cellSize = 20;
+  let gameStarted = false;
+  let score = 0;
+  let food = { x: 240, y: 200 };
+  let snake = [
+    { x: 120, y: 200 },
+    { x: 140, y: 200 },
+    { x: 160, y: 200 },
+  ];
+  let dx = cellSize; // displacement on x axis
+  let dy = 0;
+  console.log("gameBoard", gameBoard);
+  // drawScoreBoard
+  function drawScoreBoard() {
+    const scoreBoard = document.getElementById("score-board");
+    scoreBoard.textContent = score;
+  }
+  // draw div of width 20 and height 20
+  function drawDiv(x, y, classname) {
+    // console.log('drawdiv')
+    const div = document.createElement("div");
+    div.classList.add(classname);
+    div.style.top = `${y}px`;
+    div.style.left = `${x}px`;
+    return div;
+  }
+  function drawFoodAndSnake() {
+    // if anything drawn previously first clean it
+    gameBoard.innerHTML = "";
+    // wipe out everything and redraw with new coordinates when snake moves
 
-document.addEventListener('DOMContentLoaded',()=>{
-    const gameBoard=document.getElementsByClassName('game-board')[0]
-    // const snake=document.getElementsByClassName('snake')[0]
-    // const food=document.getElementsByClassName('food')[0]
-    let cellSize=20;
-    let gameStarted=false
-    let score=0
-    let food={x:300,y:200}
-    let snake=[{x:120,y:200},{x:140,y:200},{x:160,y:200}]
-    let dx=cellSize; // displacement on x axis
-    let dy=0 ;
-    console.log('gameBoard',gameBoard);
-    // drawScoreBoard
-    function drawScoreBoard() {
-        const scoreBoard=document.getElementById('score-board')
-        scoreBoard.textContent=90
+    // draw food
+    const foodElement = drawDiv(food.x, food.y, "food");
+    gameBoard.appendChild(foodElement);
 
-    }
-    // draw div
-    function drawDiv(x,y,classname){
-        // console.log('drawdiv')
-        const div =document.createElement('div');
-        div.classList.add(classname);
-        div.style.top=`${y}px`
-        div.style.left=`${x}px`
-        return div ;
-    }
-    function drawFoodAndSnake(){
-        // if anything drawn previously first clean it
-        gameBoard.innerHTML=''
-        // wipe out everything and redraw with new coordinates when snake moves
-        const foodElement=drawDiv(food.x,food.y,'food')
-        // console.log('foodElement ',foodElement)
-        gameBoard.appendChild(foodElement);
+    // draw snake
+    snake.map((snakePart) =>
+      gameBoard.appendChild(drawDiv(snakePart.x, snakePart.y, "snake"))
+    );
+
+   
+  }
+  // food movement
+  function moveFood(){
+    let newX,newY
+    do{
+        newX=Math.floor(Math.random()*((gameBoardSize-cellSize)/cellSize)*cellSize)
+        newY=Math.floor(Math.random()*((gameBoardSize-cellSize)/cellSize)*cellSize)
+
+    }while(snake.some(snakePart=>snakePart.x==newX && snakePart.y==newY))
+    food={x:newX,y:newY}
+  }
+   // check whether snake crosses wall or not (check for game over)
+   function isGameOver(){
+    console.log('before is game over snake ',snake,snake[snake.length-1].x)
+    if((snake.length==0) || (snake[0].x<=0)|| (snake[snake.length-1].x>=580)
+        || (snake[0].y<=0) || (snake[snake.length-1].y>=580) 
+        ){
+        // game over
+        console.log('is game over snake ',snake)
+        console.log(
+            (snake.length==0) , (snake[0].x<=0) , (snake[snake.length-1].x>=580) ,
+        (snake[0].y<=0) , (snake[snake.length-1].y>=580) 
+        )
+        return true
+    }   
+}
+   // snake movement
+   document.addEventListener("keydown", (event) => {
+    event.preventDefault();
+    if (event.keyCode == 39) {
+      // right
+      console.log(event.keyCode)
+     snake= snake.map((snakePart) => {
+        return {
+          ...snakePart,
+          x: snakePart.x + 20,
+        };
         
-        // console.log('draw',gameBoard.children)
-
-
+      });
     }
-    // game loop
-    function gameLoop(){
-        setInterval(()=>{
-         drawScoreBoard()
-         drawFoodAndSnake()
-        },1000)
+    if(snake[snake.length-1].x==food.x && snake[snake.length-1].y==food.y) {
+        //collison
+        score+=5
+        snake.unshift({
+            x:snake[0].x-20 ,
+            y:snake[0].y
+        })
+        // move the food
+        moveFood()
     }
-    // run game
-    function runGame(){
-   gameStarted=true
-   gameLoop()
-    }
-    //initiate game
-function initiateGame(){
-     console.log('hi')
-     const scoreBoard=document.createElement('div');
-     scoreBoard.id='score-board';
-     document.body.insertBefore(scoreBoard,gameBoard)
-    //   console.log('scoreBoard',scoreBoard)
-    runGame()
-
- }
- // call initiate game when user click on start 
- const startButton=document.getElementById('start-button')
- startButton.onclick=initiateGame
-
-  
-
+    drawScoreBoard();
+    drawFoodAndSnake();
    
-   
-})
+     // check whether snake crosses wall or not (check for game over) 
+    
+    if(isGameOver()){
+        alert(`Game Over Score =${score}`)
+        document.location.reload()
+        return 
+     }
+    //  console.log('snake',snake)
+    
+
+
+    
+    
+    
+  });
+  // game loop
+  function gameLoop() {
+    // setInterval(() => {
+      drawScoreBoard();
+      drawFoodAndSnake();
+    // }, 1000);
+  }
+  // run game
+  function runGame() {
+    gameStarted = true;
+    gameLoop();
+  }
+  //initiate game
+  function initiateGame() {
+    console.log("hi");
+    const scoreBoard = document.createElement("div");
+    scoreBoard.id = "score-board";
+    document.body.insertBefore(scoreBoard, gameBoard);
+    // console.log('scoreBoard',scoreBoard)
+    runGame();
+  }
+  // call initiate game when user click on start
+  const startButton = document.getElementById("start-button");
+  startButton.onclick = initiateGame;
+});
